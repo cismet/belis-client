cd9528775f5ea0af2c8b89eab028fb264fe76da9
Gilles Baatz <gilles.baatz@cismet.de>
2013-07-18 17:30:12 +0200

    #6 refresh key table in CidsBroker, when a key table has changed

--- src/main/java/de/cismet/belis/gui/widget/ExtendedNavigatorAttributeEditorGui.java
+++ src/main/java/de/cismet/belis/gui/widget/ExtendedNavigatorAttributeEditorGui.java
@@ -31,5 +31,6 @@ public class ExtendedNavigatorAttributeEditorGui extends NavigatorAttributeEdito
     protected void executeAfterSuccessfullSave(final CidsBean savedBean) {
         final String classname = savedBean.getMetaObject().getMetaClass().getTableName();
         CidsBroker.getInstance().fireListenerForBeanChange(classname);
+        CidsBroker.getInstance().refreshAll(classname);
     }
 }
