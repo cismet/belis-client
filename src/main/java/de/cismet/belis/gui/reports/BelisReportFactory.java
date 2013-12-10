19fddfd1780c91aa0496500d2121a2a50c8b129d
helllth <th@cismet.de>
2013-12-10 15:26:53 +0100

    working on #36: intermediate comit

--- src/main/java/de/cismet/belis/gui/reports/BelisReportFactory.java
+++ src/main/java/de/cismet/belis/gui/reports/BelisReportFactory.java
@@ -1,23 +1,51 @@
+/***************************************************
+*
+* cismet GmbH, Saarbruecken, Germany
+*
+*              ... and it just works.
+*
+****************************************************/
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package de.cismet.belis.gui.reports;
 
+import java.util.ArrayList;
+
 import de.cismet.cids.client.tools.DevelopmentTools;
+
 import de.cismet.cids.dynamics.CidsBean;
 
 /**
+ * DOCUMENT ME!
  *
- * @author thorsten
+ * @author   thorsten
+ * @version  $Revision$, $Date$
  */
 public class BelisReportFactory {
-    public static void main(String[] args) throws Exception {
-        
-        CidsBean auftrag=DevelopmentTools.createCidsBeanFromRestfulConnectionOnLocalhost("BELIS","Bearbeiter","WendlingM","kif","arbeitsauftrag",13);
-        //DevelopmentTools.showReportForBeans(null, null);
-        
+
+    //~ Methods ----------------------------------------------------------------
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @param   args  DOCUMENT ME!
+     *
+     * @throws  Exception  DOCUMENT ME!
+     */
+    public static void main(final String[] args) throws Exception {
+        final CidsBean auftrag = DevelopmentTools.createCidsBeanFromRestfulConnectionOnLocalhost(
+                "BELIS",
+                "Bearbeiter",
+                "WendlingM",
+                "kif",
+                "arbeitsauftrag",
+                13);
+        final ArrayList al = new ArrayList<CidsBean>();
+        al.add(auftrag);
+        DevelopmentTools.showReportForBeans("/de/cismet/belis/gui/search/reports/arbeitsauftrag.jasper", al);
+        System.exit(0);
     }
 }
