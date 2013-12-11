3ce949eda8a018dc50ca4b04eba168b10f16455c
helllth <th@cismet.de>
2013-12-11 17:21:29 +0100

    working on #2: Initaialize WebAccessManager with CalServerTunnel 4 external users

--- src/main/java/de/cismet/belis/client/BelisClient.java
+++ src/main/java/de/cismet/belis/client/BelisClient.java
@@ -64,6 +64,8 @@ import de.cismet.belis.broker.CidsBroker;
 
 import de.cismet.belis.util.BelisIcons;
 
+import de.cismet.cids.client.tools.CallServerTunnel;
+
 import de.cismet.cids.navigator.utils.ClassCacheMultiple;
 
 import de.cismet.cismap.commons.BoundingBox;
@@ -72,6 +74,8 @@ import de.cismet.cismap.commons.gui.statusbar.StatusBar;
 import de.cismet.cismap.commons.interaction.CismapBroker;
 
 import de.cismet.lookupoptions.gui.OptionsDialog;
+
+import de.cismet.security.WebAccessManager;
 
 import de.cismet.tools.StaticDecimalTools;
 
@@ -1141,6 +1145,7 @@ public class BelisClient extends javax.swing.JFrame implements FloatingPluginUI,
 
                 @Override
                 public void run() {
+                    WebAccessManager.getInstance().setTunnel(new CallServerTunnel("BELIS2"));
                     configManager = new ConfigurationManager();
                     configManager.setDefaultFileName(getPluginConfigurationFile());
                     configManager.setFileName(getPluginConfigurationFile());
