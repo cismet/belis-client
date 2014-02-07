1b82eebe28abf8b350df85035ef84da1041f9d2c
helllth <th@cismet.de>
2014-02-07 14:44:59 +0100

    working on #92: create a helper method that produces a jasper download

--- src/main/java/de/cismet/belis/gui/reports/BelisReporter.java
+++ src/main/java/de/cismet/belis/gui/reports/BelisReporter.java
@@ -1,12 +1,10 @@
-/**
- * *************************************************
- *
- * cismet GmbH, Saarbruecken, Germany
- * 
-* ... and it just works.
- * 
-***************************************************
- */
+/***************************************************
+*
+* cismet GmbH, Saarbruecken, Germany
+*
+*              ... and it just works.
+*
+****************************************************/
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -49,26 +47,29 @@ import de.cismet.cismap.commons.gui.printing.JasperDownload;
 import de.cismet.cismap.commons.interaction.CismapBroker;
 
 import de.cismet.tools.configuration.ConfigurationManager;
+
 import de.cismet.tools.gui.downloadmanager.DownloadManagerDialog;
 
 /**
  * DOCUMENT ME!
  *
- * @author thorsten
- * @version $Revision$, $Date$
+ * @author   thorsten
+ * @version  $Revision$, $Date$
  */
 public class BelisReporter {
 
     //~ Static fields/initializers ---------------------------------------------
+
     private static final transient org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(BelisReporter.class);
 
     //~ Methods ----------------------------------------------------------------
+
     /**
      * DOCUMENT ME!
      *
-     * @param args DOCUMENT ME!
+     * @param   args  DOCUMENT ME!
      *
-     * @throws Exception DOCUMENT ME!
+     * @throws  Exception  DOCUMENT ME!
      */
     public static void main(final String[] args) throws Exception {
         try {
@@ -103,7 +104,7 @@ public class BelisReporter {
             final ReportingArbeitsauftrag ra2 = new ReportingArbeitsauftrag();
             ra2.init(beans[1]);
 //        List l=(List)ra.getPositionenNachVeranlassung().get(ReportingArbeitsauftrag.OHNE_VERANLASSUNG);
-            final List l = (List) ra.getVeranlassungen();
+            final List l = (List)ra.getVeranlassungen();
             final ArrayList a = new ArrayList<Object>();
             a.add(ra);
             a.add(ra2);
@@ -117,39 +118,39 @@ public class BelisReporter {
 //            jasperReport = (JasperReport) JRLoader.loadObject(BelisReporter.class.getResourceAsStream(
 //                    "/de/cismet/cids/custom/reports/wunda_blau/mauer-katasterblatt.jasper"));
 
-                jasperReport = (JasperReport) JRLoader.loadObject(new File(
-                        "/Users/thorsten/dev/620-belis-client/src/main/resources/de/cismet/belis/gui/reports/arbeitsauftraege.jasper"));
+                jasperReport = (JasperReport)JRLoader.loadObject(new File(
+                            "/Users/thorsten/dev/620-belis-client/src/main/resources/de/cismet/belis/gui/reports/arbeitsauftraege.jasper"));
 
                 jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, dataSource);
 
                 EventQueue.invokeLater(new Runnable() {
 
-                    @Override
-                    public void run() {
-                        final JRViewer aViewer = new JRViewer(jasperPrint);
-                        final JFrame aFrame = new JFrame("Druckvorschau");
-                        aFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                        aFrame.getContentPane().add(aViewer);
-                        final java.awt.Dimension screenSize = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
-                        aFrame.setSize((int) (screenSize.width * 0.8), (int) (screenSize.height * 0.9f));
-                        final java.awt.Insets insets = aFrame.getInsets();
-                        aFrame.setSize(
+                        @Override
+                        public void run() {
+                            final JRViewer aViewer = new JRViewer(jasperPrint);
+                            final JFrame aFrame = new JFrame("Druckvorschau");
+                            aFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                            aFrame.getContentPane().add(aViewer);
+                            final java.awt.Dimension screenSize = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
+                            aFrame.setSize((int)(screenSize.width * 0.8), (int)(screenSize.height * 0.9f));
+                            final java.awt.Insets insets = aFrame.getInsets();
+                            aFrame.setSize(
                                 aFrame.getWidth()
-                                + insets.left
-                                + insets.right,
+                                        + insets.left
+                                        + insets.right,
                                 aFrame.getHeight()
-                                + insets.top
-                                + insets.bottom
-                                + 20);
-                        aFrame.setLocation(
+                                        + insets.top
+                                        + insets.bottom
+                                        + 20);
+                            aFrame.setLocation(
                                 (screenSize.width - aFrame.getWidth())
-                                / 2,
+                                        / 2,
                                 (screenSize.height - aFrame.getHeight())
-                                / 2);
-                        aViewer.setFitPageZoomRatio();
-                        aFrame.setVisible(true);
-                    }
-                });
+                                        / 2);
+                            aViewer.setFitPageZoomRatio();
+                            aFrame.setVisible(true);
+                        }
+                    });
             } catch (JRException ex) {
                 ex.printStackTrace();
 
@@ -160,9 +161,19 @@ public class BelisReporter {
         }
     }
 
-    public static JasperDownload getArbeitsauftragsReport(List<CidsBean> arbeitsauftraege) throws Exception{
-        ArrayList<ReportingArbeitsauftrag> repAuftraege = new ArrayList<ReportingArbeitsauftrag>(arbeitsauftraege.size());
-        for (CidsBean aa : arbeitsauftraege) {
+    /**
+     * DOCUMENT ME!
+     *
+     * @param   arbeitsauftraege  DOCUMENT ME!
+     *
+     * @return  DOCUMENT ME!
+     *
+     * @throws  Exception  DOCUMENT ME!
+     */
+    public static JasperDownload getArbeitsauftragsReport(final List<CidsBean> arbeitsauftraege) throws Exception {
+        final ArrayList<ReportingArbeitsauftrag> repAuftraege = new ArrayList<ReportingArbeitsauftrag>(
+                arbeitsauftraege.size());
+        for (final CidsBean aa : arbeitsauftraege) {
             final ReportingArbeitsauftrag ra = new ReportingArbeitsauftrag();
             ra.init(aa);
             repAuftraege.add(ra);
@@ -171,11 +182,10 @@ public class BelisReporter {
         final HashMap parameters = new HashMap();
         final JasperReport jasperReport;
         final JasperPrint jasperPrint;
-        jasperReport = (JasperReport) JRLoader.loadObject(new File(
-                "/Users/thorsten/dev/620-belis-client/src/main/resources/de/cismet/belis/gui/reports/arbeitsauftraege.jasper"));
+        jasperReport = (JasperReport)JRLoader.loadObject(new File(
+                    "/Users/thorsten/dev/620-belis-client/src/main/resources/de/cismet/belis/gui/reports/arbeitsauftraege.jasper"));
         jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, dataSource);
         final String jobname = DownloadManagerDialog.getJobname();
         return new JasperDownload(jasperPrint, jobname, "Belis-AA", "belis.aa");
     }
-
 }
